5c52745616c4c85d5e3c6adf4d404f5720902383
Jakob Skjerning <jakob@mentalized.net>
2012-10-11 16:05:48 +0200

    Extract Ruby prettifier to its own class

--- Pretty Code.py
+++ Pretty Code.py
@@ -2,6 +2,32 @@ import sublime, sublime_plugin
 import os
 import subprocess
 
+class RubyPrettifier:
+  def run(self, source):
+    pwd = os.path.join(sublime.packages_path(), 'PrettyCode')
+    ruby_interpreter = '/usr/bin/env ruby'
+
+    prettifier_path = os.path.join(pwd, 'lib', 'ruby-beautify')
+    prettifier_script = '%(path)s/bin/rbeautify' % {'path': prettifier_path}
+
+    # Build the command to run with Popen. TODO: There must be a better way to build
+    # the command other than string manipulation. It seems to be a recipe for cross-OS
+    # issues.
+    prettifier_command = ''
+    prettifier_command += ruby_interpreter
+    prettifier_command += ' -I "' + prettifier_path + '/lib' + '"'
+    prettifier_command += ' "' + prettifier_script + '"'
+
+    proc = subprocess.Popen(
+      prettifier_command,
+      shell=True,
+      stdin=subprocess.PIPE,
+      stderr=subprocess.PIPE,
+      stdout=subprocess.PIPE
+    )
+    output = proc.communicate(source)[0]
+    return output
+
 class PrettifyCodeCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     if not self.supported_syntax(self.syntax()):
@@ -29,29 +55,8 @@ class PrettifyCodeCommand(sublime_plugin.TextCommand):
 
   def prettified_region(self, region):
     """Returns the prettified version of region."""
-    pwd = os.path.join(sublime.packages_path(), 'PrettyCode')
-    ruby_interpreter = '/usr/bin/env ruby'
 
-    prettifier_path = os.path.join(pwd, 'lib', 'ruby-beautify')
-    prettifier_script = '%(path)s/bin/rbeautify' % {'path': prettifier_path}
-
-    # Build the command to run with Popen. TODO: There must be a better way to build
-    # the command other than string manipulation. It seems to be a recipe for cross-OS
-    # issues.
-    prettifier_command = ''
-    prettifier_command += ruby_interpreter
-    prettifier_command += ' -I "' + prettifier_path + '/lib' + '"'
-    prettifier_command += ' "' + prettifier_script + '"'
-
-    proc = subprocess.Popen(
-      prettifier_command,
-      shell=True,
-      stdin=subprocess.PIPE,
-      stderr=subprocess.PIPE,
-      stdout=subprocess.PIPE
-    )
-    output = proc.communicate(self.view.substr(region))[0]
-    return output
+    return RubyPrettifier().run(self.view.substr(region))
 
   def region_with_all_text(self):
     """Returns a region covering all text in the currently active view."""
