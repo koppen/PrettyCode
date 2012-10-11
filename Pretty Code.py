b2a2ed7a6d7e7a04cad3c0c39c7d99a19762e29b
Jakob Skjerning <jakob@mentalized.net>
2012-10-11 15:09:18 +0200

    Proof of concept using ruby-beautifier

--- Pretty Code.py
+++ Pretty Code.py
@@ -1,5 +1,6 @@
 import sublime, sublime_plugin
 import os
+import subprocess
 
 class PrettifyCodeCommand(sublime_plugin.TextCommand):
   def run(self, edit):
@@ -15,8 +16,29 @@ class PrettifyCodeCommand(sublime_plugin.TextCommand):
 
   # Returns the prettified version of region
   def prettified_region(self, region):
-    result = os.popen('pwd').read()
-    return result
+    pwd = os.path.join(sublime.packages_path(), 'PrettyCode')
+    ruby_interpreter = '/usr/bin/env ruby'
+
+    prettifier_path = os.path.join(pwd, 'lib', 'ruby-beautify')
+    prettifier_script = '%(path)s/bin/rbeautify' % {'path': prettifier_path}
+
+    # Build the command to run with Popen. TODO: There must be a better way to build
+    # the command other than string manipulation. It seems to be a recipe for cross-OS
+    # issues.
+    prettifier_command = ''
+    prettifier_command += ruby_interpreter
+    prettifier_command += ' -I "' + prettifier_path + '/lib' + '"'
+    prettifier_command += ' "' + prettifier_script + '"'
+
+    proc = subprocess.Popen(
+      prettifier_command,
+      shell=True,
+      stdin=subprocess.PIPE,
+      stderr=subprocess.PIPE,
+      stdout=subprocess.PIPE
+    )
+    output = proc.communicate(self.view.substr(region))[0]
+    return output
 
   def view(self):
     return self.edit.active_view()
